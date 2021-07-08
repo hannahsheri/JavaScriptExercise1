a7d058e755ebb120bf25b8979ce5bd7b54663e5d
Hannah <83435725+hannahsheri@users.noreply.github.com>
2021-07-08 19:17:57 -0400

    Add files via upload

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+let actualTemp="75";
+let desiredTemp="70";
+
+if ("desiredTemp" == "actualTemp") {
+    let outcome = "Standby";
+} else if ("desiredTemp" < "actualTemp") {
+    let outcome = "Run A/C";
+} else if ("desiredTemp" > "actualTemp"){
+    let outcome = "Run heat";
+}
+//this is a test
+
